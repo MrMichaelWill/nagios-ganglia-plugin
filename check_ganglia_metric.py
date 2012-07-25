4d503cd05366fe48f03adf034fa2396a77e9389c
MrMichaelWill <MrMichaelWill@gmail.com>
2012-07-25 16:28:36 -0700

    removed complication of having tiny function in external lib file, pulled it into the main file

--- check_ganglia_metric.py
+++ check_ganglia_metric.py
@@ -28,7 +28,27 @@
 import re
 import optparse
 
-from lib.networkutils import netcat
+import socket
+
+def netcat(hostname, port, content):
+    """
+    @param hostname: str
+    @param port: int
+    @param content: str
+    @return data: str
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    data = ''
+    while 1:
+        cur_data = s.recv(1024)
+        if cur_data == "":
+            break
+        data += str(cur_data)
+    s.close()
+    return data
 
 VALUE_PARSING_RE = r'VAL=\"(.*?)\"'
 
